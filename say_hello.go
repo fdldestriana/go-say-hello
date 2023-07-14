19a6419a98a1a63ca710748ad8a1acb7d22f68c9
fadlydestriana <fadlydestriana@gmail.com>
2023-07-14 21:32:13 +0700

    update say hello

--- say_hello.go
+++ say_hello.go
@@ -1,8 +1,12 @@
 package go_say_hello
 
 func SayHello() string {
-	return "Hello"
+	return "Hello World"
 }
 
 // setelah selesai membuat satu module, kita bisa merilisnya ke github
 // di golang biasanya tag diawali dengan v, contoh v1.0.0
+
+// lakukan
+// git tag v1.0.0
+// git push origin v1.0.0
